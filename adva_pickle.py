e4ad2eae0a8d4538fae8dc0b58c13086ef159b2f
asdfLiang <LiangZj9624@163.com>
2024-10-05 01:55:08 +0800

    feat: pickle 序列化

---
+++ adva_pickle.py
@@ -0,0 +1,17 @@
+import pickle
+
+"""
+    序列化
+"""
+
+# 示例数据
+data = {"name": "John", "age": 30, "isStudent": False, "grades": [85, 90, 78, 92]}
+
+# 使用 pickle 保存数据
+with open("resources/pkl_data.json", "wb") as file:
+    pickle.dump(data, file)
+
+# 使用 pickle 加载数据
+with open("resources/pkl_data.json", "rb") as file:
+    load_data = pickle.load(file)
+print(load_data)
