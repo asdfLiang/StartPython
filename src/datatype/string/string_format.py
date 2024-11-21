5b2e05ed1e1632cada26f93d5725c71a5a95afe4
asdfLiang <liangzj9624@163.com>
2024-11-21 16:09:58 +0800

    线性代数

---
+++ src/datatype/string/string_format.py
@@ -0,0 +1,6 @@
+# % 形式输出
+name = "alice"
+age = 20
+print("My name is %s, %d years old" % (name, age))
+
+# 禁止转换
---
+++ src/datatype/string/string_init.py
@@ -0,0 +1,17 @@
+str1 = "123"
+str2 = "467"
+str3 = """
+    row1
+    row2
+    row3
+"""
+str4 = str("abcd")
+str5 = "\n"
+str6 = r"\n"
+
+print(f"str1 = {str1}")
+print(f"str2 = {str2}")
+print(f"str3 = {str3}")
+print(f"str4 = {str4}")
+print(f"str5 = {str5}")
+print(f"str6 = {str6}")
